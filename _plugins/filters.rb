6155a48938e3fa888819cbc2612081df3047ee6e
Juozas Kaziukenas <juozas@juokaz.com>
2011-05-29 16:00:09 +0100

    Post keywords and description are added to meta tags

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -3,5 +3,8 @@ module Jekyll
     def summarize(str, splitstr = '<!--more-->')
       str.split(splitstr)[0]
     end
+    def description(str, splitstr = '<!--more-->')
+      return Maruku.new(str.split(splitstr)[0]).to_html.gsub(/<\/?[^>]*>/, "")
+    end
   end
 end
