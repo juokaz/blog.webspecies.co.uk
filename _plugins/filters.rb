2bc83ef7dac09f48b5069fa598fd2e0af27ef36f
Juozas Kaziukenas <juozas@juokaz.com>
2011-06-07 13:05:17 +0100

    Summary doesn't include images or anything else out of basic HTML tags

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,10 +1,13 @@
 module Jekyll
   module Filters
     def summarize(str, splitstr = '<!--more-->')
-      str.split(splitstr)[0]
+      return _strip_html(str.split(splitstr)[0], ['a','p','strong','em'])
     end
     def description(str, splitstr = '<!--more-->')
-      return Maruku.new(str.split(splitstr)[0]).to_html.gsub(/<\/?[^>]*>/, "")
+      return Maruku.new(str.split(splitstr)[0]).to_html.gsub(/<\/?[^>]*>/, "").strip!
+    end
+    def _strip_html(str, allowed = ['a','p','img','br','i','b','u','ul','li'])
+      return str.gsub(/<(\/|\s)*[^(#{allowed.join('|') << '|\/'})][^>]*>/,'')
     end
   end
 end
