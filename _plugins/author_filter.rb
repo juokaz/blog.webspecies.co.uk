a59c0df3af886d1bc4e9cd8982bca44bd7d4a2b0
Juozas Kaziukenas <juozas@juokaz.com>
2013-03-12 05:08:29 +1100

    Removed author plugin

--- _plugins/author_filter.rb
+++
@@ -1,9 +0,0 @@
-module Author
-  def author(input)
-    author = input.downcase.sub(" ", "-") + '.html'
-    profile = File.read(File.join('_includes', 'author_' + author))
-    return Maruku.new(profile).to_html.sub("<p>", "").sub("</p>", "")
-  end
-end
-
-Liquid::Template.register_filter(Author)
